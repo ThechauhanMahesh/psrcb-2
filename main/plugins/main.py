d4e7b3b3b09f9afe60379bed8b0b582553e94939
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-11-30 00:23:25 +0530

    Update main.py

--- main/plugins/main.py
+++ main/plugins/main.py
@@ -51,7 +51,7 @@ async def clone(event):
         await set_timer(event, process, timer) 
         return 
     if 't.me' in link and not 't.me/c/' in link:
-        await get_msg(None, Bot, event.sender_id, edit.id, link, 0)
+        await get_msg(None, Bot, Drone, event.sender_id, edit.id, link, 0)
         await set_timer(event, process, timer) 
     if 't.me/c/' in link:
         userbot = ""
@@ -68,5 +68,5 @@ async def clone(event):
                 return await edit.edit(errorC)
         else:
             return await edit.edit("Your login credentials not found.")
-        await get_msg(userbot, Bot, event.sender_id, edit.id, link, 0)
+        await get_msg(userbot, Bot, Drone,event.sender_id, edit.id, link, 0)
         await set_timer(event, process, timer) 
