7e919b161965fcb8f3a75ba2061a638b8d2deecd
bloodlustyt <67737806+bloodlustyt@users.noreply.github.com>
2023-01-13 22:15:45 +0530

    Update main.py

--- main/plugins/main.py
+++ main/plugins/main.py
@@ -55,7 +55,7 @@ async def clone(event):
     if f'{int(event.sender_id)}' in user:
         return await edit.edit("Please don't spam links, wait until ongoing process is done.")
     user.append(f'{int(event.sender_id)}')
-    if "tg://openmessage?user_id=" in link:
+    if (str(link)).lower().startswith("tg://openmessage?user_id="):
         x, t = check_timer(event.sender_id, process, timer, 60) 
         if x == False:
             ind = user.index(f'{int(event.sender_id)}')
