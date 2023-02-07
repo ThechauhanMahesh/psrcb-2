172e27b2e120435178bd622cea3376025672414c
bloodlustyt <67737806+bloodlustyt@users.noreply.github.com>
2023-02-07 17:49:06 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -11,7 +11,7 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 # variables
 API_ID = 4796990
 API_HASH = "32b6f41a4bf740efed2d4ce911f145c7"
-BOT_TOKEN = "5832484897:AAGSsZLed3xHeCkD53csvh6laOQN_uFl12s"
+BOT_TOKEN = "5147519086:AAFHiBwjXPkHgkiPcVLs0tRK5mNtzg10XoU"
 FORCESUB = int("-1001711957758")
 ACCESS = int("-1001879806908")
 MONGODB_URI = "mongodb+srv://Vasusen:darkmaahi@cluster0.o7uqb.mongodb.net/cluster0?retryWrites=true&w=majority"
