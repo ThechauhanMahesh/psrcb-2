cbf0d6de9527455952890a39cb51753ae00aabb0
ThechauhanMahesh <125787905+ThechauhanMahesh@users.noreply.github.com>
2023-04-10 22:58:06 +0530

    Update pyroplug.py

--- main/plugins/pyroplug.py
+++ main/plugins/pyroplug.py
@@ -172,7 +172,7 @@ async def get_msg(userbot, client, bot, sender, to, edit_id, msg_link, i):
             return 
         except Exception as e:
             print(e)
-            if "messages.SendMedia" in str(e):
+            if "messages.SendMedia" in str(e) or "'NoneType' object is not subscriptable" in str(e):
                 try: 
                     if "mp4" in file.split("."):
                         UT = time.time()
