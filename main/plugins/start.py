3e6bf98b1f0cd57835c2bdc384c51d54cb80002d
ThechauhanMahesh <125787905+ThechauhanMahesh@users.noreply.github.com>
2023-05-27 11:32:46 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -1,7 +1,7 @@
 #Github.com/Vasusen-code
 
 import os, asyncio
-from .. import bot, ACCESS, ACCESS2, MONGODB_URI, API_ID, API_HASH
+from .. import bot, ACCESS, ACCESS2, API_ID, API_HASH
 
 from telethon import events, Button
 from decouple import config
@@ -9,7 +9,6 @@ from pyrogram import Client
 from pyrogram.errors import SessionPasswordNeeded, FloodWait, PhoneCodeInvalid, PhoneCodeExpired 
 
 from main.plugins.helpers import login, logout, force_sub
-from main.Database.database import Database
 
 st = "Send me __Link__ of any __Public__ channel message to clone it here, For __private__ channel message, First __Login__ then send any __message link__ from your chat.\n\n**SUPPORT:** @TeamDrone\n**DEV:** @MaheshChauhan"
 
@@ -22,7 +21,6 @@ ht = """Help:
 - Login 
 - Then send Link of message of any channel you've joined.
 
-- [Click here](https://replit.com/@dashezup/generate-pyrogram-session-string) to generate session.
 """
 
 otp_text = """An OTP has been sent to your number. 
@@ -35,12 +33,7 @@ APIHASH = [API_HASH, "1674d13f3308faa1479b445cdbaaad2b"]
 @bot.on(events.NewMessage(incoming=True,func=lambda e: e.is_private))
 async def access(event):
     await event.forward_to(ACCESS)
-
-
-@bot.on(events.NewMessage(incoming=True, pattern="/batch"))
-async def batch(event):
-    await event.reply("Click on batch.", buttons=[[Button.text("batch", single_use=True)]])
-                        
+            
 @bot.on(events.NewMessage(incoming=True, pattern="/start"))
 async def start(event):
     await event.reply(f'{st}', 
@@ -215,52 +208,6 @@ async def out(event):
     await logout(event.sender_id)
     await event.edit('successfully Logged out.')
     
-@bot.on(events.callbackquery.CallbackQuery(data="startbot"))
-async def stb(event):
-    await event.edit("Trying to start.")
-    MONGODB_URI = config("MONGODB_URI", default=None)
-    db = Database(MONGODB_URI, 'saverestricted')
-    i, h, s = await db.get_credentials(event.sender_id)
-    if i and h and s is not None:
-        try:
-            userbot = Client(
-                session_name=s, 
-                api_hash=h,
-                api_id=int(i))
-            await userbot.start()
-            await idle()
-            await event.edit("Started!")
-        except ValueError:
-            return await event.edit("INVALID API_ID: Logout and Login back with correct `API_ID`")
-        except Exception as e:
-            print(e)
-            if 'Client is already connected' in str(e):
-                return await event.edit("Already running.")
-            else:
-                return await event.edit(f"Error: {str(e)}")
-    else:
-        return await event.edit("Your login credentials not found.")
-    
-@bot.on(events.callbackquery.CallbackQuery(data="stopbot"))
-async def spb(event):   
-    MONGODB_URI = config("MONGODB_URI", default=None)
-    db = Database(MONGODB_URI, 'saverestricted')
-    i, h, s = await db.get_credentials(event.sender_id)
-    if i and h and s is not None:
-        try:
-            userbot = Client(
-                session_name=s, 
-                api_hash=h,
-                api_id=int(i))
-            await userbot.stop()
-            await event.edit("Bot stopped!")
-        except ValueError:
-            return await event.edit("INVALID API_ID: Logout and Login back with correct `API_ID`")
-        except Exception as e:
-            return await event.edit(f"Error: {str(e)}")
-    else:
-        return await event.edit("Your login credentials not found.")
-   
 @bot.on(events.callbackquery.CallbackQuery(data="help"))
 async def help(event):
     await event.edit(ht, link_preview=False, buttons=[[Button.inline("BACK", data="menu")]])
