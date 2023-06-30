cd68a2e21d7329831f228482bc990efe5a52e454
ThechauhanMahesh <125787905+ThechauhanMahesh@users.noreply.github.com>
2023-06-30 15:04:40 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -17,6 +17,7 @@ ACCESS = int("-1001879806908")
 ACCESS2 = int("-1001823465454")
 MONGODB_URI = "mongodb+srv://Vasusen:darkmaahi@cluster0.o7uqb.mongodb.net/cluster0?retryWrites=true&w=majority"
 AUTH_USERS = 5351121397
+BOT_UN = "PremiumSRCB_DC2Bot"
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
