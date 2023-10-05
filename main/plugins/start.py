5a3f0445c719c40865dd9e2f34238b941704f090
ThechauhanMahesh <125787905+ThechauhanMahesh@users.noreply.github.com>
2023-10-05 20:55:04 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -47,9 +47,10 @@ APIHASH = [API_HASH, "1674d13f3308faa1479b445cdbaaad2b"]
 async def access(event):
     await event.forward_to(ACCESS)
 
-@bot.on(events.NewMessage(incoming=True, pattern="/start"))
-async def start(event):
+@bot.on(events.NewMessage(incoming=True, pattern="/help"))
+async def help(event):
     await event.reply("click on below button to watch tutorial video", buttons=[[Button.url("CLICK HERE", url="https://t.me/SaveRestricted_Content/14")]])
+    
 @bot.on(events.NewMessage(incoming=True, pattern="/start"))
 async def start(event):
     await event.reply(f'{st}', 
