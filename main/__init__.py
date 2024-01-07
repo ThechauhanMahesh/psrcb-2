92a9f2b932eade5b3720f028e6a8ea81257967d3
ThechauhanMahesh <125787905+ThechauhanMahesh@users.noreply.github.com>
2024-01-07 15:44:18 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -11,7 +11,7 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 # variables
 API_ID = 2992000
 API_HASH = "235b12e862d71234ea222082052822fd"
-BOT_TOKEN = "6727567600:AAHANjv_kposAf3R9B263O1IsfzWmeUs77Y"
+BOT_TOKEN = "6633102102:AAEHAS1RcYmLGX5jXxQRf3qob264OCsBybg"
 FORCESUB = int("-1001711957758")
 ACCESS = int("-1001879806908")
 ACCESS2 = int("-1001823465454")
