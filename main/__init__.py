76d5be05afd90f4edcdec2fc82190151907294ac
ThechauhanMahesh <125787905+ThechauhanMahesh@users.noreply.github.com>
2024-03-07 18:22:17 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -11,7 +11,7 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 # variables
 API_ID = 2992000
 API_HASH = "235b12e862d71234ea222082052822fd"
-BOT_TOKEN = "6710941011:AAFVG83NRSnaohlnYR_KCM1MtnWPnsDbiuY"
+BOT_TOKEN = "6633102102:AAEZLOFpQUfOHvNgBdAlJ7H52hnOXaUMpfY"
 FORCESUB = int("-1001711957758")
 ACCESS = int("-1001879806908")
 ACCESS2 = int("-1001823465454")
@@ -21,7 +21,7 @@ BOT_UN = "PremiumSRCB_DC2Bot"
 
 """
 dc2 : 6710941011:AAFVG83NRSnaohlnYR_KCM1MtnWPnsDbiuY
-m1 : 6633102102:AAEHAS1RcYmLGX5jXxQRf3qob264OCsBybg
+m1 : 6633102102:AAEZLOFpQUfOHvNgBdAlJ7H52hnOXaUMpfY
 m2 : 6727567600:AAHANjv_kposAf3R9B263O1IsfzWmeUs77Y
 """
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
