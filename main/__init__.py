77ca78c92a9f8c058caaf63d3e48ed1644d0f7a1
ThechauhanMahesh <125787905+ThechauhanMahesh@users.noreply.github.com>
2024-03-09 11:38:57 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -24,7 +24,7 @@ FORCESUB = int("-1001711957758")
 ACCESS = int("-1001879806908")
 ACCESS2 = int("-1001823465454")
 MONGODB_URI = "mongodb+srv://Vasusen:darkmaahi@cluster0.o7uqb.mongodb.net/cluster0?retryWrites=true&w=majority"
-AUTH_USERS = 5351121397
+AUTH_USERS = 1807573686
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
