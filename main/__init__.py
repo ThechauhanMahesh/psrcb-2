cd00081137bf72d054066d12fc71cc64923f05c0
Mahesh Chauhan <125787905+ThechauhanMahesh@users.noreply.github.com>
2024-05-09 21:32:29 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -1,56 +1,60 @@
 #ChauhanMahesh/Vasusen/DroneBots/COL
 
 from pyrogram import Client
-from telethon import TelegramClient
-# from decouple import config
-import logging, time, sys
+import logging, sys
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 
-"""
-ps2 : 6890628049:AAG4LctwPDIIHYgG-RRDHD_BCPV67c36ut8
-ps1 : 6900940299:AAEOPMETfJZrwwKvsx-5A9oFuv_nVs4jVq0
-ps3 : 6757308177:AAEe364a_hMLcR4JFymFVXNYghEws7szoh8
-ps4 : 6608701591:AAFERzaluRyq4dNHDQi5EdBNlLXffMeJdbQ
-ps5 : 6669926453:AAEZc_j2UETA96mOgXTCWCmoi4RMfcdoycg
-"""
-
 # variables
 API_ID = 2992000
 API_HASH = "235b12e862d71234ea222082052822fd"
-BOT_TOKEN = "6900940299:AAEOPMETfJZrwwKvsx-5A9oFuv_nVs4jVq0"
+BOT_TOKEN = "5558723055:AAF6Dw7otUWetI5m-lNPAnblZ68kaNKB14w"
 FORCESUB = int("-1001711957758")
 ACCESS = int("-1001879806908")
-ACCESS2 = int("-1001823465454")
 MONGODB_URI = "mongodb+srv://thechauhanmahesh:XgbFpSEe3pM9P45z@cluster0.mkaomd0.mongodb.net"
 AUTH_USERS = 1807573686
 
-bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
-uploader_ubot = Client(
-  "uploader_ubot",
-  session_string="BQAtp4AAhtF1yIVdl0YOozgeVFGIo9_EFzMLLLYvOgJpyfyo0FNsUmNN41TG5IMbZt7vVIfHJnDYqJ1xzDKHi9SD2-wRmeHYIiBkpDP4ziJ2aVdPWuiX6aSyW5wCQdSiuhNgAlT6wTVsR8Fh9LwliU65W2Aic_1eFAvlpD-EgvGr7KWm67J6xrv0FGSH6ZHncV-OBaWMCK3iO2dvGgKef47e6a1KGulBsjjehh4xla2gKGTPLaWD06AqvvQ3q9pjwrevw7GiSkpCC0yFwD85M5Yr3wIkiNMXh6ndOe4bDTpSG4_se7nxKQP5TbxxfmjaqKqByuVWrzuD9cinns7HaxEgndswVQAAAABrvWK2AA", 
-  api_hash=API_HASH, 
-  api_id=API_ID, 
-  max_concurrent_transmissions=5
-) 
+help_text = """Help:
 
+**FOR PUBLIC CHANNEL:**
+- Send me direct link of message. 
 
-try:
-    uploader_ubot.start()
-except BaseException:
-    print("Userbot Error !")
+**FOR PRIVATE CHANNEL:**
+- Login 
+- Then send Link of message of any channel you've joined.
 
-Bot = Client(
-    "SaveRestricted",
+#FAQ 
+
+- If bot says “Have you joined the channel?” Then just login again in bot and try.
+
+- If bot says “Please don't spam links, wait until ongoing process is done.” then send /free command in bot and try after 10 minutes. 
+
+- if bot says “Login credentials not found” the just login again
+
+- If bot shows error containing “AUTH_KEY_DUPLICATED” in it then login again.
+
+- if you batch is stuck then use /cancel 
+
+#Note : Don't use the /free command unnecessarily.
+"""
+
+otp_text = """An OTP has been sent to your number. 
+
+Please send the OTP with space, example: `1 2 3 4 5`."""
+
+bot = Client(
+    "PyrogamBot", 
+    api_hash=API_HASH, 
+    api_id=API_ID, 
     bot_token=BOT_TOKEN,
-    api_id=int(API_ID),
-    api_hash=API_HASH
-)    
+    workers=343
+    ) # pyrogram bot
 
-try:
-    Bot.start()
-except Exception as e:
-    print(e)
-    sys.exit(1)
+# try:
+#     bot.start()
+# except Exception as e:
+#     print(e)
+#     sys.exit()
+
