b0407b7a60f80a921037bb8d8c40531aee60496e
Mahesh Chauhan <125787905+ThechauhanMahesh@users.noreply.github.com>
2024-05-09 21:30:37 +0530

    Update batch.py

--- main/plugins/batch.py
+++ main/plugins/batch.py
@@ -7,167 +7,148 @@ Plugin for both public & private channels!
 
 import asyncio
 
-from .. import bot as Drone, Bot, AUTH_USERS  as AUTH
+from .. import bot as Drone
 from main.plugins.pyroplug import get_bulk_msg
 from main.plugins.helpers import get_link, set_subscription, check_subscription
 from main.Database.database import db
 
-from telethon import events, Button
-from pyrogram import Client 
+from pyrogram import Client, filters, types
 from pyrogram.errors import FloodWait
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 
 errorC = """Error: Couldn't start client by Login credentials, Please logout and login again."""
 
-conversation = []
+batch_link = False
 
-@Drone.on(events.NewMessage(incoming=True, pattern='/cancel'))
-async def cancel(event):
-    async with event.client.conversation(event.sender_id, exclusive=False) as conv:
-        await conv.cancel_all()
-    if not (await db.get_process(event.sender_id))["batch"]:
-        return await event.reply("No batch active, all previous conversations are cancelled.")
-    await db.rem_process(event.sender_id)
-    async with event.client.conversation(event.sender_id, exclusive=False) as conv:
-        await conv.cancel_all()
-    await event.reply("✅ Done.")
+@Drone.on_message(filters=filters.command('cancel') & filters.incoming)
+async def cancel(_, message: types.Message):
+    if not (await db.get_process(message.from_user.id))["batch"]:
+        return await message.reply("No batch active, all previous conversations are cancelled.")
+    await db.rem_process(message.from_user.id)
+    await message.reply("✅ Done.")
 
-@Drone.on(events.NewMessage(incoming=True, pattern='/myplan'))
-async def check_plan(event):
-    data = await db.get_data(event.sender_id)
-    await event.reply(f'**DATE OF SUBSCRIPTION:** {data["dos"]}\n**DATE OF EXPIRY:** {data["doe"]}\n**PLAN:** {data["plan"]}')
+@Drone.on_message(filters=filters.command('myplan') & filters.incoming)
+async def myplan(_, message: types.Message):
+    data = await db.get_data(message.from_user.id)
+    await message.reply(f'**DATE OF SUBSCRIPTION:** {data["dos"]}\n**DATE OF EXPIRY:** {data["doe"]}\n**PLAN:** {data["plan"]}')
     return
 
-@Drone.on(events.NewMessage(incoming=True, from_users=AUTH, pattern='/ss'))
-async def ss(event):
-    edit = await event.reply("Processing...")
-    msg = await event.get_reply_message()
-    data = str(msg.text).split(" ")
+@Drone.on_message(filters=filters.command('x') & filters.incoming)
+async def ss(_, message: types.Message):
+    edit = await message.reply("Processing...")
+    msg = message.reply_to_message
+    data = str(msg.text).split("")
     date = data[1]
     if date == "None":
         date = False
     await set_subscription(int(data[0]), date, int(data[2]), data[3])
     x = await db.get_data(int(data[0]))
     await edit.edit(f'{x}')
-    await event.client.send_message(int(data[0]), "Your plan is active now, send /myplan to check.")
+    await Drone.send_message(int(data[0]), "Your plan is active now, send /myplan to check.")
     return 
 
-@Drone.on(events.NewMessage(incoming=True, pattern='/caption'))
-async def _caption(event):
-    if not (await db.get_data(event.sender_id))["plan"] == "pro":
-        await event.reply("⚠️ Purchase pro plan.")
-    await event.reply("Choose an action", buttons=[[Button.inline("OFF", data="off")],
-                                                   [Button.inline("ADD", data="add")],
-                                                   [Button.inline("DELETE", data="delete")],
-                                                   [Button.inline("REPLACE", data="replace")]])
+@Drone.on_message(filters=filters.command('caption') & filters.incoming)
+async def caption(_, message: types.Message):
+    if not (await db.get_data(message.from_user.id))["plan"] == "pro":
+        return await message.reply("⚠️ Purchase pro plan.")
+    await message.reply(
+        "Choose an action", 
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton("OFF", "off")],
+                [InlineKeyboardButton("ADD", "add")],
+                [InlineKeyboardButton("DELETE", "delete")],
+                [InlineKeyboardButton("REPLACE", "replace")]
+            ]
+        )
+    )
 
-@Drone.on(events.callbackquery.CallbackQuery(data="add"))
-async def _add(event):
-    await event.delete()
-    async with Drone.conversation(event.chat_id, exclusive=False) as conv: 
-        await conv.send_message("Send the text you want to add in captions.")
-        try:
-            x = await conv.get_response()
-        except:
-            await conv.send_message("Cannot wait longer for your response.")
-        await db.add_caption(event.sender_id, x.text)
-        await conv.send_message("Done ✅")
+@Drone.on_callback_query(filters.regex(r"add"))
+async def add(_, cb: CallbackQuery):
+    await cb.delete()
+    user_id = cb.from_user.id
+    add = await Drone.ask(user_id, "Send the text you want to add in captions.", filters=filters.text)
+    await db.add_caption(user_id, add.text)
+    await Drone.send_message(user_id, "Done ✅")
         
-@Drone.on(events.callbackquery.CallbackQuery(data="delete"))
-async def delete(event):
-    await event.delete()
-    async with Drone.conversation(event.chat_id, exclusive=False) as conv: 
-        await conv.send_message("Send the text you want to delete in captions.")
-        try:
-            x = await conv.get_response()
-        except:
-            await conv.send_message("Cannot wait longer for your response.")
-        await db.delete_caption(event.sender_id, x.text)
-        await conv.send_message("Done ✅")
+@Drone.on_callback_query(filters.regex(r"delete"))
+async def delete(_, cb: CallbackQuery):
+    await cb.delete()
+    user_id = cb.from_user.id
+    delete = await Drone.ask(user_id, "Send the text you want to delete in captions.", filters=filters.text)
+    await db.delete_caption(user_id, delete.text)
+    await Drone.send_message(user_id, "Done ✅")
         
-@Drone.on(events.callbackquery.CallbackQuery(data="off"))
-async def off(event):
-    await event.delete()
-    await db.disable_caption(event.sender_id)
-    await event.client.send_message(event.sender_id, "No changes will be made in captions ✅")
+@Drone.on_callback_query(filters.regex(r"off"))
+async def off(_, cb: CallbackQuery):
+    await cb.delete()
+    user_id = cb.from_user.id
+    await db.disable_caption(user_id)
+    await Drone.send_message(user_id, "No changes will be made in captions ✅")
     
-@Drone.on(events.callbackquery.CallbackQuery(data="replace"))
-async def replace(event):
-    await event.delete()
-    async with Drone.conversation(event.chat_id, exclusive=False) as conv: 
-        await conv.send_message("Send the text you want to replace in captions.")
-        try:
-            text1 = await conv.get_response()
-        except:
-            await conv.send_message("Cannot wait longer for your response.")
-        await conv.send_message("Send the text you want to replace by in captions.")
-        try:
-            text2 = await conv.get_response()
-        except:
-            await conv.send_message("Cannot wait longer for your response.")
-        await db.replace_caption(event.sender_id, {"d":text1.text, "a":text2.text})
-        await conv.send_message("Done ✅")
+@Drone.on_callback_query(filters.regex(r"replace"))
+async def replace(_, cb: CallbackQuery):
+    await cb.delete()
+    user_id = cb.from_user.id
+    delete = await Drone.ask(user_id, "Send the text you want to replace in captions.", filters=filters.text)
+    replace = await Drone.ask(user_id, "Send the text you want to replace by in captions.", filters=filters.text)
+    await db.replace_caption(user_id, {"d":delete.text, "a":replace.text})
+    await Drone.send_message(user_id, "Done ✅")
         
-@Drone.on(events.NewMessage(incoming=True, pattern='/batch'))
-async def _batch(event):
-    if not event.is_private:
+@Drone.on_message(filters=filters.command('batch') & filters.incoming & filters.private)
+async def batch(client, message: types.Message):
+    global batch_link
+    user_id = message.from_user.id
+
+    await check_subscription(user_id)
+
+    if (await db.get_data(user_id))["plan"] == "basic":
+        await message.reply("⚠️ Buy Monthly subscription or Pro subscription.")
         return
-    await check_subscription(event.sender_id)
-    if (await db.get_data(event.sender_id))["plan"] == "basic":
-        await event.reply("⚠️ Buy Monthly subscription or Pro subscription.")
-        return
-    pr = (await db.get_process(event.sender_id))["process"]
+    
+    pr = (await db.get_process(user_id))["process"]
     if pr:
-        return await event.reply("⚠️ You've already started one process, wait for it to complete!")
-    async with Drone.conversation(event.chat_id, exclusive=False) as conv: 
-        if pr != True:
-            await conv.send_message("Send me the message link you want to start saving from, as a reply to this message.", buttons=Button.force_reply())
-            try:
-                link = await conv.get_reply()
-                try:
-                    _link = get_link(link.text)
-                except Exception:
-                    await conv.send_message("⚠️ No link found.")
-                    return await conv.cancel_all()
-            except Exception as e:
-                print(e)
-                await conv.send_message("Cannot wait more longer for your response!")
-                return await conv.cancel_all()
-            await conv.send_message("Send me the number of files/range you want to save from the given message, as a reply to this message.", buttons=Button.force_reply())
-            try:
-                _range = await conv.get_reply()
-            except Exception as e:
-                print(e)
-                await conv.send_message("Cannot wait more longer for your response!")
-                return await conv.cancel_all()
-            try:
-                value = int(_range.text)
-                if value > 30:
-                    if not (await db.get_data(event.sender_id))["plan"] == "pro":
-                        await conv.send_message("⚠️ You can only get upto 30 files in a single batch.")
-                        return await conv.cancel_all()
-                    elif value > 100:
-                        await conv.send_message("⚠️ You can only get upto 100 files in a single batch.")
-                        return await conv.cancel_all()
-            except ValueError:
-                await conv.send_message("Range must be an integer!")
-                return await conv.cancel_all()
-            i, h, s = await db.get_credentials(event.chat.id)
-            chat = await db.get_chat(event.chat.id)
-            if chat == None:
-                chat = event.sender_id
-            userbot = None
-            if i and h and s is not None:
-                userbot = Client("saverestricted", session_string=s, api_hash=h, api_id=int(i))     
-            else:
-                await conv.send_message("⚠️ Your login credentials not found.")
-                return await conv.cancel_all()
-            await db.update_process(event.sender_id, batch=True)
-            await run_batch(userbot, Bot, event.sender_id, chat, _link, value) 
-            await conv.cancel_all()
-            await db.rem_process(event.sender_id)
+        return await message.reply("⚠️ You've already started one process, wait for it to complete!")
+    
+    batch_link = True
+    link = await Drone.ask(user_id, "Send me the message link you want to start saving from, as a reply to this message.", filters=filters.text)
+    try:
+        link = get_link(link.text)
+    except Exception:
+        return await message.reply("⚠️ No link found.")
+    batch_link = False
+    
+    range = await Drone.ask(user_id, "Send me the number of files/range you want to save from the given message, as a reply to this message.", filters=filters.text)
+    try:
+        range = range.text
+        value = int(range)
+        if value > 30:
+            if not (await db.get_data(user_id))["plan"] == "pro":
+                await message.reply("⚠️ You can only get upto 30 files in a single batch.")
+                return
+            elif value > 100:
+                await message.reply("⚠️ You can only get upto 100 files in a single batch.")
+                return 
+    except ValueError:
+        await message.reply("Range must be an integer!")
+        return
             
-async def run_batch(userbot, client, sender, chat, link, _range):
-    for i in range(_range):
+    i, h, s = await db.get_credentials(user_id)
+    chat = await db.get_chat(user_id)
+    if chat == None:
+        chat = user_id
+    userbot = None
+    if s:
+        userbot = Client("saverestricted", session_string=s, api_hash=h, api_id=int(i))     
+    else:
+        await message.reply("⚠️ Your login credentials not found.")
+        return
+    await db.update_process(user_id, batch=True)
+    await run_batch(userbot, client, user_id, chat, link, value) 
+    await db.rem_process(user_id)
+            
+async def run_batch(userbot, client, sender, chat, link, value):
+    for i in range(value):
         if i < 50:
             timer = 10
         elif i > 25 and i < 50:
