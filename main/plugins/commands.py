e76b85f0a14986d84c28625b308d561c43a75325
Mahesh Chauhan <125787905+ThechauhanMahesh@users.noreply.github.com>
2024-05-10 09:37:17 +0530

    Update commands.py

--- main/plugins/commands.py
+++ main/plugins/commands.py
@@ -1,11 +1,11 @@
-#Github.com/Vasusen-code
+# Github.com/Vasusen-code
 
 import os, asyncio
 from .. import bot as Drone, API_ID, API_HASH, help_text as ht, otp_text, AUTH_USERS
 
 from pyrogram import Client, filters, types
 from pyrogram.errors import SessionPasswordNeeded, FloodWait, PhoneCodeInvalid, PhoneCodeExpired 
-
+from pyromod.exceptions import ListenerTimeout 
 from main.plugins.helpers import login_credentials, logout_credentials
 from main.Database.database import db
 
@@ -46,27 +46,15 @@ async def login(_, message: types.Message):
     session, passcode, ai, ah= None, None, None, None
     user_id = message.from_user.id
 
-    contact = await Drone.ask(user_id, "Send me your contact number with country code(eg +1 or +91) to login.", filters=filters.text)
-    number = ' '.join(str(contact.text))
-
-    code_alert = await message.reply("Sending code...")
-
-    ai = APIID[0]
-    ah = APIHASH[0]
-    client = Client(f"{user_id}_account", api_id=ai, api_hash=ah, in_memory=True)
     try:
-        await client.connect()
-    except ConnectionError:
-        await client.disconnect()
-        await client.connect()
-    try:
-        code = await client.send_code(number)
-        await asyncio.sleep(1)
-    except FloodWait:
-        await client.disconnect()
-        client = Client("my_account", api_id=APIID[-1], api_hash=APIHASH[-1])
-        ai = APIID[-1]
-        ah = APIHASH[-1]
+        contact = await Drone.ask(user_id, "Send me your contact number with country code(eg +1 or +91) to login.", filters=filters.text, timeout=60)
+        number = ' '.join(str(contact.text))
+
+        code_alert = await message.reply("Sending code...")
+
+        ai = APIID[0]
+        ah = APIHASH[0]
+        client = Client(f"{user_id}_account", api_id=ai, api_hash=ah, in_memory=True)
         try:
             await client.connect()
         except ConnectionError:
@@ -75,37 +63,51 @@ async def login(_, message: types.Message):
         try:
             code = await client.send_code(number)
             await asyncio.sleep(1)
-        except FloodWait as e:
-            await code_alert.edit(f"Can't send code, you have Floodwait of {e.x} Seconds.")
-            return
-        except Exception as e:
-            print(e)
-            await code_alert.edit(f"**Error**: {str(e)}")
-            return
-        await code_alert.delete()
+        except FloodWait:
+            await client.disconnect()
+            client = Client("my_account", api_id=APIID[-1], api_hash=APIHASH[-1])
+            ai = APIID[-1]
+            ah = APIHASH[-1]
+            try:
+                await client.connect()
+            except ConnectionError:
+                await client.disconnect()
+                await client.connect()
+            try:
+                code = await client.send_code(number)
+                await asyncio.sleep(1)
+            except FloodWait as e:
+                await code_alert.edit(f"Can't send code, you have Floodwait of {e.x} Seconds.")
+                return
+            except Exception as e:
+                print(e)
+                await code_alert.edit(f"**Error**: {str(e)}")
+                return
+            await code_alert.delete()
 
-    ask_code = await Drone.ask(user_id, otp_text, filters=filters.text)
-    otp = ask_code.text
-    try:
-        await client.sign_in(number, code.phone_code_hash, phone_code=' '.join(str(otp)))
-    except PhoneCodeInvalid:
-        await message.reply("Invalid Code, try again.")
-        return
-    except PhoneCodeExpired:
-        await message.reply("Code has expired, try again.")
-        return
-    except SessionPasswordNeeded:
-        two_step = await Drone.ask(user_id, "Send your Two-Step Verification password.", filters=filters.text)
-        passcode = two_step.text
+        ask_code = await Drone.ask(user_id, otp_text, filters=filters.text, timeout=60)
+        otp = ask_code.text
         try:
-            await client.check_password(passcode)
+            await client.sign_in(number, code.phone_code_hash, phone_code=' '.join(str(otp)))
+        except PhoneCodeInvalid:
+            await message.reply("Invalid Code, try again.")
+            return
+        except PhoneCodeExpired:
+            await message.reply("Code has expired, try again.")
+            return
+        except SessionPasswordNeeded:
+            two_step = await Drone.ask(user_id, "Send your Two-Step Verification password.", filters=filters.text, timeout=60)
+            passcode = two_step.text
+            try:
+                await client.check_password(passcode)
+            except Exception as e:
+                await message.reply(f"**ERROR:** {str(e)}")
+                return
         except Exception as e:
             await message.reply(f"**ERROR:** {str(e)}")
             return
-    except Exception as e:
-        await message.reply(f"**ERROR:** {str(e)}")
-        return
-    
+    except ListenerTimeout:
+        return await message.reply("You took too long to respond.")
     try:
         session = await client.export_session_string()
     except Exception as e:
@@ -129,7 +131,10 @@ async def help(_, message: types.Message):
 @Drone.on_message(filters=filters.command('setthumb') & filters.incoming)
 async def setthumb(client, message: types.Message):   
     user_id = message.from_user.id
-    image = await Drone.ask(user_id, "Send me any image for thumbnail.", filters=filters.photo)              
+    try:
+        image = await Drone.ask(user_id, "Send me any image for thumbnail.", filters=filters.photo, timeout=60)   
+    except ListenerTimeout:
+        return await message.reply("You took too long to respond.")      
     edit = await message.reply("Trying to download..")
     path = await client.download_media(image)
     if os.path.exists(f'{user_id}.jpg'):
@@ -145,3 +150,7 @@ async def remthumb(_, message: types.Message):
         await edit.edit('✅ Removed!')
     except Exception:
         await edit.edit("No thumbnail was saved.")     
+
+
+
+
