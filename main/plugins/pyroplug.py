4f0c7c92012f86d652bff7f429b4f28cec2d848a
Mahesh Chauhan <125787905+ThechauhanMahesh@users.noreply.github.com>
2024-08-09 19:49:24 +0530

    Update pyroplug.py

--- main/plugins/pyroplug.py
+++ main/plugins/pyroplug.py
@@ -13,7 +13,7 @@ def thumbnail(sender):
     else:
          return None
       
-async def get_msg(userbot, client:Client, sender, to, editable_msg, msg_link, i=0, plan="basic"):
+async def get_msg(userbot, client:Client, sender, to, editable_msg, msg_link, caption_data, i=0, plan="basic"):
 
     file, file_size, chat, caption, thumb_path, upload_client = None, None, None, None, thumbnail(sender), client
 
@@ -24,10 +24,9 @@ async def get_msg(userbot, client:Client, sender, to, editable_msg, msg_link, i=
     
     if 't.me/c/' in msg_link or 't.me/b/' in msg_link:
         if 't.me/b/' in msg_link:
-            chat = str(msg_link.split("/")[-2])
-        else:
-            chat = int('-100' + str(msg_link.split("/")[-2]))
-
+            chat = str(msg_link.split("/b/")[1].split("/")[0])
+        elif 't.me/c/' in msg_link:
+            chat = int('-100' + str(msg_link.split("/c/")[1]).split("/")[0])
         try:
             msg = await userbot.get_messages(chat, msg_id)
             
@@ -45,18 +44,6 @@ async def get_msg(userbot, client:Client, sender, to, editable_msg, msg_link, i=
                     await editable_msg.delete()
                     return
                 else:
-                    if msg.video:
-                        file_size = msg.video.file_size
-                    else:
-                        file_size = msg.document.file_size
-                    if file_size > 2097152000:
-                        if plan != "pro":
-                            return await editable_msg.edit("Buy pro plan and telegram premium to upload file size over 2Gb.")
-                        else:
-                            upload_client = userbot
-                            if to == sender:
-                                me = await client.get_me()
-                                to = me.username
                     downloaded, update = await download(userbot, msg, editable_msg)
                     if not downloaded:
                         if not update:
@@ -69,21 +56,53 @@ async def get_msg(userbot, client:Client, sender, to, editable_msg, msg_link, i=
 
             if msg.caption is not None:
                 caption = msg.caption
+                if plan == "pro":
+                    new_caption = ""
+                    action = caption_data["action"]
+                    string = caption_data["string"]
+                    if action is not None:
+                        if action == "add":
+                            new_caption = caption + f"\n\n{string}"
+                        if action == "delete":
+                            new_caption = caption.replace(string, "")
+                        if action == "replace":
+                            new_caption = caption.replace(string["d"], string["a"])
+                        caption = new_caption
+            else:
+                if plan == "pro":
+                    action = caption_data["action"]
+                    if action == "add":
+                        caption = caption_data["string"]
 
             await editable_msg.edit("Preparing to upload...")
+
+            if file == None:
+                return await editable_msg.edit(f'❌ Failed to save: `{msg_link}`\n\nThis link is not downloadble.')
+                
+            file_size = os.path.getsize(file)
+            
+            if file_size > 2097152000:
+                if plan != "pro":
+                    return await editable_msg.edit("Buy pro plan and telegram premium to upload file size over 2Gb.")
+                else:
+                    upload_client = userbot
+                    if to == sender:
+                        me = await client.get_me()
+                        to = me.username
             
             uploaded, update = await upload(upload_client, file, to, msg, editable_msg, thumb_path=thumb_path, caption=caption)
             if uploaded:
                 await editable_msg.delete()
             else:
                 if not update:
-                    return await get_msg(userbot, client, sender, to, editable_msg, msg_link, i=0, plan=plan)
+                    return await get_msg(userbot, client, sender, to, editable_msg, msg_link, caption_data, i=0, plan=plan)
                 else:
                     return await editable_msg.edit(f"❌ Failed to upload: `{msg_link}`\n\nError: {update}")
                     
         except Exception as e:
             print(e)
-
+            return await editable_msg.edit(f'❌ Failed to save: `{msg_link}`\n\nError: {str(e)}')
+            
         await editable_msg.delete()
 
     else:
@@ -94,9 +113,8 @@ async def get_msg(userbot, client:Client, sender, to, editable_msg, msg_link, i=
         try:
             msg = await client.get_messages(chat, msg_id)
             if msg.empty:
-                group = await userbot.get_chat(chat)
-                group_link = f't.me/c/{int(group.id)}/{int(msg_id)}'
-                return await get_msg(userbot, client, sender, to, editable_msg, group_link, i=0, plan=plan)
+                group_link = f't.me/b/{chat}/{int(msg_id)}'
+                return await get_msg(userbot, client, sender, to, editable_msg, group_link, caption_data, i=0, plan=plan)
             else:
                 await client.copy_message(to, chat, msg_id)
         except Exception as e:
